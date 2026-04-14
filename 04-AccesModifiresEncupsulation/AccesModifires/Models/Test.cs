eebee966ac5a4bbd0715ec5ea9c055e79b55d257
EzetQelenderli <azatrg-apa202@code.edu.az>
2026-04-14 23:51:12 +0400

    first commit

---
+++ 04-AccesModifiresEncupsulation/AccesModifires/Models/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+
+namespace AccesModifires.Models
+{
+    internal class Test
+    {
+        public Test()
+        {
+            Car car = new Car();
+        }
+    }
+}
