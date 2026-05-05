4b95dbaf55b547d3eaa612645c004f85044b0b02
EzetQelenderli <azatrg-apa202@code.edu.az>
2026-05-05 12:59:46 +0400

    first commit

---
+++ 25-MVC Intro/MVCProject/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return Content("APA202");
+            //var Student = new JsonResult(new { id = 1, name = "Ali", surname = "quliyev" });
+            return View("Index");
+        }
+        public IActionResult Detail(int? id)
+        {
+            if (id is null || id < 1)
+            {
+                return RedirectToAction(nameof(Error));
+            }
+            return RedirectToAction(nameof(Index),"Product");
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ 25-MVC Intro/MVCProject/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCProject.Controllers
+{
+    public class ProductController:Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
